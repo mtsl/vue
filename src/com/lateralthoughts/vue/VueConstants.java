97f43da93f3355b86c2c5619f51756f1edf8c5aa
krishnav <krishna@23neem.com>
2013-06-25 11:49:42 +0530

    FB and Google+ Integration 0n 25 JUNE 2013

---
+++ src/com/lateralthoughts/vue/VueConstants.java
@@ -0,0 +1,17 @@
+package com.lateralthoughts.vue;
+
+/**
+ * Here we will declare all constants related to Vue application.
+ * @author krishna
+ *
+ */
+public class VueConstants {
+
+        public static final String SHAREDPREFERENCE_NAME = "VuePreferences";
+        public static final String FIRSTTIME_LOGIN_PREFRENCE_FLAG = "firstTimeLoginPrefrenceFlag";
+        public static final String CREATED_AISLE_COUNT_IN_PREFERENCE = "createdAisleCountInPreference";
+        public static final String COMMENTS_COUNT_IN_PREFERENCES = "commentsCountInPreferences";
+        public static final int CREATE_AISLE_LIMIT_FOR_LOGIN = 5;
+        public static final int COMMENTS_LIMIT_FOR_LOGIN = 10;
+
+}
