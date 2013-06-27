7d829f778da777012e04634c3ee8bc790b488886
suru_indra <surendra@23neem.com>
2013-06-27 12:12:53 +0530

    commit

---
+++ src/com/lateralthoughts/vue/utils/FbGPlusDetails.java
@@ -0,0 +1,29 @@
+package com.lateralthoughts.vue.utils;
+/**
+ * This is class is used to store the facebook friends information.
+ * @author krishna
+ *
+ */
+public class FbGPlusDetails {
+
+	private String name = null;
+	private String profile_image_url = null;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfile_image_url() {
+		return profile_image_url;
+	}
+	public void setProfile_image_url(String profile_image_url) {
+		this.profile_image_url = profile_image_url;
+	}
+	public FbGPlusDetails(String name, String profile_image_url) {
+		this.name = name;
+		this.profile_image_url = profile_image_url;
+	}
+
+}
