e03b1b55c3b3f30511c4c5c2460e545b64a9b1e8
suru_indra <surendra@23neem.com>
2013-07-17 18:29:08 +0530

    showing items from db only if there is now network, using thread pool with only one thread to get the data from local db and to save the new data got from server to local db.

--- src/com/lateralthoughts/vue/VueLandingAislesFragment.java
+++ src/com/lateralthoughts/vue/VueLandingAislesFragment.java
@@ -162,7 +162,7 @@ public class VueLandingAislesFragment extends SherlockFragment/*Fragment*/ {
                 mRightColumnView.invalidateViews();
             }
         }
-        
+
         @Override
         public void onScroll(AbsListView view, int firstVisibleItem,
                 int visibleItemCount, int totalItemCount) {
@@ -201,8 +201,8 @@ public class VueLandingAislesFragment extends SherlockFragment/*Fragment*/ {
                 }
                 
             }
-            
-          /*  if(VueTrendingAislesDataModel.getInstance(mContext).loadOnRequest) {
+
+            if(VueTrendingAislesDataModel.getInstance(mContext).loadOnRequest) {
               int lastVisiblePosition = firstVisibleItem + visibleItemCount;
               int totalItems = 0;
               if (view.equals(mLeftColumnView) ){
@@ -213,7 +213,7 @@ public class VueLandingAislesFragment extends SherlockFragment/*Fragment*/ {
               if((totalItems - lastVisiblePosition) < 5) {
                 VueTrendingAislesDataModel.getInstance(mContext).loadMoreAisles();
               }
-            }*/
+            }
             
         }
     };
