cd2cbb4ca64ef811e10923956eb07f8372ab1468
krishnav <krishna@23neem.com>
2013-08-12 14:43:01 +0530

    Added new class for adding sorting images based on Height and width

---
+++ src/com/lateralthoughts/vue/utils/SortBasedOnImageWidthHeight.java
@@ -0,0 +1,13 @@
+package com.lateralthoughts.vue.utils;
+
+import java.util.Comparator;
+
+public class SortBasedOnImageWidthHeight implements
+		Comparator<OtherSourceImageDetails> {
+	@Override
+	public int compare(OtherSourceImageDetails lhs, OtherSourceImageDetails rhs) {
+		return lhs.getWidthHeightMultipliedValue()
+				- rhs.getWidthHeightMultipliedValue();
+	}
+
+}
