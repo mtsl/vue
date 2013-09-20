952fea13fc427e322459da83a4584f6404eeeee8
suru_indra <surendra@23neem.com>
2013-09-20 18:58:28 +0530

    minor changes

--- src/com/lateralthoughts/vue/connectivity/TrendingAislesContentParser.java
+++ src/com/lateralthoughts/vue/connectivity/TrendingAislesContentParser.java
@@ -17,128 +17,113 @@ import com.lateralthoughts.vue.VueTrendingAislesDataModel;
 import com.lateralthoughts.vue.parser.Parser;
 
 public class TrendingAislesContentParser extends ResultReceiver {
-	private int mState;
+  private int mState;
 
-	// this variable above is usually set to one of the following values
+  // this variable above is usually set to one of the following values
 
-	public TrendingAislesContentParser(Handler handler, int reciverSource) {
-		super(handler);
-		mState = reciverSource;
-	}
+  public TrendingAislesContentParser(Handler handler, int reciverSource) {
+    super(handler);
+    mState = reciverSource;
+  }
 
-	@Override
-	protected void onReceiveResult(int resultCode, final Bundle resultData) {
-		switch (mState) {
-		case VueConstants.AISLE_TRENDING_LIST_DATA:
+  @Override
+  protected void onReceiveResult(int resultCode, final Bundle resultData) {
+    switch (mState) {
+      case VueConstants.AISLE_TRENDING_LIST_DATA:
 
-			Thread t = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					final ArrayList<AisleWindowContent> aislesList = new Parser()
-							.parseTrendingAislesResultData(
-									resultData.getString("result"),
-									resultData.getBoolean("loadMore"));
-					/*
-					 * if (aislesList != null && aislesList.size() > 0) { int
-					 * size = aislesList.size(); Log.i("ailsesize",
-					 * "ailseListSize: " + size); new
-					 * DbDataSetter(aislesList).execute(); aislesList = null; }
-					 */
-					Log.i("ailsesize", "ailseListSizemaintrending: "
-							+ aislesList.size());
-					if (VueLandingPageActivity.landingPageActivity != null) {
-						VueLandingPageActivity.landingPageActivity
-								.runOnUiThread(new Runnable() {
+        Thread t = new Thread(new Runnable() {
+          @Override
+          public void run() {
+            final ArrayList<AisleWindowContent> aislesList = new Parser()
+                .parseTrendingAislesResultData(resultData.getString("result"),
+                    resultData.getBoolean("loadMore"));
 
-									@Override
-									public void run() {
-										VueTrendingAislesDataModel.getInstance(VueApplication.getInstance()).loadOnRequest = true;
-										if (aislesList != null
-												&& aislesList.size() > 0) {
-											for (int i = 0; i < aislesList
-													.size(); i++) {
-												VueTrendingAislesDataModel
-														.getInstance(
-																VueApplication
-																		.getInstance())
-														.addItemToList(
-																aislesList
-																		.get(i)
-																		.getAisleContext().mAisleId,
-																aislesList
-																		.get(i));
-											}
+            if (aislesList != null && aislesList.size() > 0) {
+              int size = aislesList.size();
+              Log.i("ailsesize", "ailseListSize: " + size);
+              new DbDataSetter(aislesList).execute();
+            }
 
-											VueTrendingAislesDataModel
-													.getInstance(
-															VueApplication
-																	.getInstance())
-													.dismissProgress();
-											// if this is the first set of data
-											// we
-											// are receiving
-											// go
-											// ahead
-											// notify the data set changed
-											VueTrendingAislesDataModel
-													.getInstance(
-															VueApplication
-																	.getInstance())
-													.dataObserver();
-										}
-									}
-								});
-					}
-					/*
-					 * TrendingAislesContentParser mTrendingAislesParser = new
-					 * TrendingAislesContentParser( new Handler(), 1);
-					 * mTrendingAislesParser.send(1, null);
-					 */
-				}
-			});
-			t.start();
-			break;
-		case VueConstants.AISLE_TRENDING_PARSED_DATA:
-			VueTrendingAislesDataModel
-					.getInstance(VueApplication.getInstance())
-					.dismissProgress();
-			// if this is the first set of data we are receiving
-			// go
-			// ahead
-			// notify the data set changed
-			VueTrendingAislesDataModel
-					.getInstance(VueApplication.getInstance()).dataObserver();
-			break;
-		default:
-			// we should never have to encounter this!
-			break;
-		}
-	}
+            Log.i("ailsesize",
+                "ailseListSizemaintrending: " + aislesList.size());
+            if (VueLandingPageActivity.landingPageActivity != null) {
+              VueLandingPageActivity.landingPageActivity
+                  .runOnUiThread(new Runnable() {
 
-	private class DbDataSetter extends AsyncTask<Void, Void, Void> {
-		ArrayList<AisleWindowContent> mAislesList;
+                    @Override
+                    public void run() {
+                      VueTrendingAislesDataModel.getInstance(VueApplication
+                          .getInstance()).loadOnRequest = true;
+                      if (aislesList != null && aislesList.size() > 0) {
+                        for (int i = 0; i < aislesList.size(); i++) {
+                          VueTrendingAislesDataModel.getInstance(
+                              VueApplication.getInstance()).addItemToList(
+                              aislesList.get(i).getAisleContext().mAisleId,
+                              aislesList.get(i));
+                        }
 
-		public DbDataSetter(ArrayList<AisleWindowContent> aislesList) {
-			mAislesList = aislesList;
-		}
+                        VueTrendingAislesDataModel.getInstance(
+                            VueApplication.getInstance()).dismissProgress();
+                        // if this is the first set of data
+                        // we
+                        // are receiving
+                        // go
+                        // ahead
+                        // notify the data set changed
+                        VueTrendingAislesDataModel.getInstance(
+                            VueApplication.getInstance()).dataObserver();
+                      }
+                    }
+                  });
+            }
+            /*
+             * TrendingAislesContentParser mTrendingAislesParser = new
+             * TrendingAislesContentParser( new Handler(), 1);
+             * mTrendingAislesParser.send(1, null);
+             */
+          }
+        });
+        t.start();
+        break;
+      case VueConstants.AISLE_TRENDING_PARSED_DATA:
+        VueTrendingAislesDataModel.getInstance(VueApplication.getInstance())
+            .dismissProgress();
+        // if this is the first set of data we are receiving
+        // go
+        // ahead
+        // notify the data set changed
+        VueTrendingAislesDataModel.getInstance(VueApplication.getInstance())
+            .dataObserver();
+        break;
+      default:
+        // we should never have to encounter this!
+        break;
+    }
+  }
 
-		@Override
-		protected void onPreExecute() {
-			// TODO Auto-generated method stub
-			super.onPreExecute();
-		}
+  private class DbDataSetter extends AsyncTask<Void, Void, Void> {
+    ArrayList<AisleWindowContent> mAislesList;
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			DataBaseManager.addTrentingAislesFromServerToDB(
-					VueApplication.getInstance(), mAislesList);
-			return null;
-		}
+    public DbDataSetter(ArrayList<AisleWindowContent> aislesList) {
+      mAislesList = aislesList;
+    }
 
-		@Override
-		protected void onPostExecute(Void result) {
-			mAislesList.clear();
-			super.onPostExecute(result);
-		}
-	}
+    @Override
+    protected void onPreExecute() {
+      super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+      DataBaseManager.addTrentingAislesFromServerToDB(
+          VueApplication.getInstance(), mAislesList);
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+      mAislesList.clear();
+      super.onPostExecute(result);
+    }
+  }
 }
