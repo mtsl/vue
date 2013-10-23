63bf150dfe4567850f95ea75bdaee7bfaa27ff0a
Raju Kunche <raju.k@23neem.com>
2013-10-23 15:11:17 +0530

    minor changes

--- src/com/lateralthoughts/vue/VueApplication.java
+++ src/com/lateralthoughts/vue/VueApplication.java
@@ -162,8 +162,8 @@ public class VueApplication extends Application {
 		}
 
 		// R.drawable.aisle_content_empty;
-		Crittercism.init(getApplicationContext(), CRITTERCISM_APP_ID,
-				crittercismConfig);
+		/*Crittercism.init(getApplicationContext(), CRITTERCISM_APP_ID,
+				crittercismConfig);*/
 
 	}
 
