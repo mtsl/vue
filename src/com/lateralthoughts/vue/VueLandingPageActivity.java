f3638ac96dacb9e0137f1b02d486dcaccfb524ce
Raju Kunche <raju.k@23neem.com>
2013-11-20 17:40:36 +0530

    progressdialog xml added

--- src/com/lateralthoughts/vue/VueLandingPageActivity.java
+++ src/com/lateralthoughts/vue/VueLandingPageActivity.java
@@ -90,7 +90,7 @@ public class VueLandingPageActivity extends BaseActivity {
 	public static String mOtherSourceAddImageAisleId = null;
 	private static final String TRENDING_SCREEN_VISITORS = "Trending_Screen_Visitors";
 	public static Activity landingPageActivity = null;
-    private 	 Dialog dialog;
+     
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -866,9 +866,6 @@ public class VueLandingPageActivity extends BaseActivity {
 		@Override
 		public void dismissProgress(boolean fromWhere) {
 			mLoadProgress.setVisibility(View.INVISIBLE);
-			if(dialog != null)
-			dialog.dismiss();
-	 
 			if (mFragment == null) {
 				mFragment = (VueLandingAislesFragment) getSupportFragmentManager()
 						.findFragmentById(R.id.aisles_view_fragment);
@@ -1324,13 +1321,6 @@ public class VueLandingPageActivity extends BaseActivity {
 
 	}
 	
-	private void showDialog(){
-		/*if(dialog != null){*/
-	    dialog = new Dialog(this, R.style.Theme_Dialog_Translucent);
-		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		dialog.setContentView(R.layout.progressdialog);
-		/*}*/
-		dialog.show();
-	}
+ 
 
 }
