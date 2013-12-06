93ad079befa4a4675582327c8fdc7a06d66ea436
Raju Kunche <raju.k@23neem.com>
2013-12-06 12:33:40 +0530

    comment class added

---
+++ src/com/lateralthoughts/vue/domain/ImageCommentRequest.java
@@ -0,0 +1,50 @@
+package com.lateralthoughts.vue.domain;
+
+public class ImageCommentRequest {
+	Long id;
+	String imageCommentOwnerFirstName;
+	String imageCommentOwnerLastName;
+	Long createdTimestamp;
+	String imageCommentOwnerImageURL;
+
+	Long ownerImageId;
+
+	public Long getOwnerImageId() {
+		return ownerImageId;
+	}
+
+	public void setOwnerImageId(Long ownerImageId) {
+		this.ownerImageId = ownerImageId;
+	}
+
+	Long ownerUserId;
+
+	public Long getOwnerUserId() {
+		return ownerUserId;
+	}
+
+	public void setOwnerUserId(Long ownerUserId) {
+		this.ownerUserId = ownerUserId;
+	}
+
+	String comment;
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	Long lastModifiedTimestamp;
+
+	public Long getLastModifiedTimestamp() {
+		return lastModifiedTimestamp;
+	}
+
+	public void setLastModifiedTimestamp(Long lastModifiedTimestamp) {
+		this.lastModifiedTimestamp = lastModifiedTimestamp;
+	}
+
+}
