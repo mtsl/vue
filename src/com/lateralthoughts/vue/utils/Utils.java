da2d403369363787bb87c27f72f8469eaa62d790
krishnav <krishna@23neem.com>
2013-12-10 12:55:58 +0530

    Conflict commit

--- src/com/lateralthoughts/vue/utils/Utils.java
+++ src/com/lateralthoughts/vue/utils/Utils.java
@@ -367,11 +367,11 @@ public class Utils {
 	 * 
 	 * @return
 	 */
-	/*public static String date() {
-		SimpleDateFormat dateFormatGmt = new SimpleDateFormat(
-				VueConstants.DATE_FORMAT);
-		return dateFormatGmt.format(new Date());
-	}*/
+	/*
+	 * public static String date() { SimpleDateFormat dateFormatGmt = new
+	 * SimpleDateFormat( VueConstants.DATE_FORMAT); return
+	 * dateFormatGmt.format(new Date()); }
+	 */
 
 	/**
 	 * 7 * 24 * 60 * 60 * 1000
@@ -382,7 +382,8 @@ public class Utils {
 		long twoWeeksDifferenceTime = (System.currentTimeMillis() - (7 * 24 * 60 * 60 * 1000));
 		SimpleDateFormat dateFormatGmt = new SimpleDateFormat(
 				VueConstants.DATE_FORMAT);
-		return twoWeeksDifferenceTime + "";//dateFormatGmt.format(new Date(twoWeeksDifferenceTime));
+		return twoWeeksDifferenceTime + "";// dateFormatGmt.format(new
+											// Date(twoWeeksDifferenceTime));
 	}
 
 	public static Bitmap getBitmap() {
@@ -783,12 +784,14 @@ public class Utils {
 				R.style.Theme_Dialog_Translucent);
 		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		dialog.setContentView(R.layout.vue_popup);
+		View networkdialogline = dialog.findViewById(R.id.networkdialogline);
 		TextView noButton = (TextView) dialog.findViewById(R.id.nobutton);
 		TextView okButton = (TextView) dialog.findViewById(R.id.okbutton);
 		TextView messagetext = (TextView) dialog.findViewById(R.id.messagetext);
 		messagetext.setText("Sorry, Server side integration is pending.");
 		okButton.setText("OK");
 		noButton.setVisibility(View.GONE);
+		networkdialogline.setVisibility(View.GONE);
 		okButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				dialog.dismiss();
