af460f91287b0e17729a9ef0cc75019f36bdebcc
Raju Kunche <raju.k@23neem.com>
2013-12-20 18:02:41 +0530

    added about strings and cleand up the code

--- src/gcm/com/vue/android/gcmclient/GCMBroadcastReceiver.java
+++ src/gcm/com/vue/android/gcmclient/GCMBroadcastReceiver.java
@@ -1,12 +1,5 @@
 package gcm.com.vue.android.gcmclient;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Calendar;
-
 import gcm.com.vue.gcm.notifications.GCMClientNotification;
 import gcm.com.vue.gcm.notifications.GCMClientNotification.NotificationType;
 import gcm.com.vue.gcm.notifications.RestOperationTypeEnum;
@@ -16,9 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Environment;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -53,7 +44,6 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
                     .getString("data");
             if (broadcastMessage != null
                     && broadcastMessage.trim().length() > 0) {
-                writeToSdcard(broadcastMessage);
                 final GCMClientNotification lastReceivedNotification = (new ObjectMapper())
                         .readValue(broadcastMessage,
                                 GCMClientNotification.class);
@@ -105,14 +95,13 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
                                                             Context.NOTIFICATION_SERVICE);
                                             Intent notificationIntent = new Intent(
                                                     VueApplication
-                                                    .getInstance(),
-                                            VueLandingPageActivity.class);
-                                            notificationIntent.putExtra("notfication", "MyAisles");
+                                                            .getInstance(),
+                                                    VueLandingPageActivity.class);
+                                            notificationIntent.putExtra(
+                                                    "notfication", "MyAisles");
                                             PendingIntent contentIntent = PendingIntent
-                                                    .getActivity(
-                                                            VueApplication
-                                                                    .getInstance(),
-                                                            0,
+                                                    .getActivity(VueApplication
+                                                            .getInstance(), 0,
                                                             notificationIntent,
                                                             0);
                                             NotificationCompat.Builder builder = new NotificationCompat.Builder(
@@ -148,39 +137,11 @@ public class GCMBroadcastReceiver extends BroadcastReceiver {
                                     .add(userGetRequest);
                         }
                     }
-                } else {
-                    writeToSdcard("Receiving null from receiver");
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         
-    }
-    private void writeToSdcard(String message) {
-        
-        String path = Environment.getExternalStorageDirectory().toString();
-        File dir = new File(path + "/vueGCMLogs/");
-        if (!dir.isDirectory()) {
-            dir.mkdir();
-        }
-        File file = new File(dir, "/"
-                + Calendar.getInstance().get(Calendar.DATE) + ".txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(
-                    new FileWriter(file, true)));
-            out.write("\n" + message + "\n");
-            out.flush();
-            out.close();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
